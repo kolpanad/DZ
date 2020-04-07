c42d8c23a515c23ed54bf9b7a95d8e4d4b1ddb0f
Надя Попцова <kolpanad@MacBook-Air-Nada.local>
2020-04-07 02:07:12 -0500

    Initial commit

---
+++ src/CalculateRomanNumbers.java
@@ -0,0 +1,23 @@
+public class CalculateRomanNumbers extends Caculator{
+    public static void calculateRomanNum(int a, String operator, int b) {
+        int c;
+        if (operator.equals("+")) {
+            c = plus(a, b);
+            ConvertRomanNumbers.arabToRoman(c);
+        } else if (operator.equals("-")) {
+            c = subtract(a, b);
+            ConvertRomanNumbers.arabToRoman(c);
+        } else if (operator.equals("*")) {
+            c = multiply(a, b);
+            ConvertRomanNumbers.arabToRoman(c);
+        } else if (operator.equals("/")) {
+            try {
+                c = divide(a, b);
+                ConvertRomanNumbers.arabToRoman(c);
+            } catch (ArithmeticException e) {
+                System.out.println("На ноль делить нельзя");
+            }
+
+        }
+    }
+}
